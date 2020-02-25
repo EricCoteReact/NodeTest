0bfa994690be4a61bd94db4f8d1cfdf3ef4b9950
Eric Cote <EricCote@ReactAcademy.ca>
2020-02-25 12:06:23 -0500

    read the querystring!

--- first.js
+++ first.js
@@ -2,8 +2,6 @@ import http from 'http';
 //import URL from 'url';
 
 function hello(req, res) {
-  console.dir(req, { depth: 0 });
-
   let url = new URL('http://localhost' + req.url);
 
   switch (url.pathname) {
@@ -14,7 +12,8 @@ function hello(req, res) {
       res.write('<h1>Hello World<h1>');
       break;
     case '/test':
-      res.write('<h1>This test Page<h1>');
+      let amount = url.searchParams.get('amount');
+      res.write(`<h1>This test Page with amount ${amount} <h1>`);
       break;
     default:
       res.write('<h1>this is error 404</h1>');
