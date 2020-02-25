4ff2fe9f24b606e6f3436f4eb6aac24b1c9ecec2
Eric Cote <EricCote@ReactAcademy.ca>
2020-02-25 11:50:17 -0500

    url parsed

--- first.js
+++ first.js
@@ -1,7 +1,25 @@
 import http from 'http';
+//import URL from 'url';
 
 function hello(req, res) {
-  res.write('<h1>Hello World!</h1>');
+  console.dir(req, { depth: 0 });
+
+  let url = new URL('http://localhost' + req.url);
+
+  switch (url.pathname) {
+    case '/':
+      res.write('<h1>This is the homePage<h1>');
+      break;
+    case '/hello':
+      res.write('<h1>Hello World<h1>');
+      break;
+    case '/test':
+      res.write('<h1>This test Page<h1>');
+      break;
+    default:
+      res.write('<h1>this is error 404</h1>');
+      break;
+  }
   res.end();
 }
 
